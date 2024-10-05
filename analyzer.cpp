af5b0ea4c07357a244de37880a36ca04dd101a42
HexagonalExpanse <99370485+HexagonalExpanse@users.noreply.github.com>
2024-10-05 17:05:42 -0400

    fixed merge conflict

--- analyzer.cpp
+++ analyzer.cpp
@@ -13,7 +13,7 @@ int main() {
     std::vector<std::string> wordList = getStringData();
     test_algorithm(&wordList, "not_here");
     test_algorithm(&wordList, "mzzzz");
-    test_algorithm(&wordList, "aaaaa22");
+    test_algorithm(&wordList, "aaaaa");
     return 0;
 }
 
